8e878297c53e44cbfe77282446e9f9219fae1a89
square <fxz@huizuche.com>
2016-08-22 16:00:38 +0800

    添加回调函数和验证结果返回值

--- m-validate.js
+++ m-validate.js
@@ -29,25 +29,39 @@
         this.form = document.forms[form];
         this.options = [];
         this.valid()
-    }
+    };
 
     Mvalidate.prototype = {
         add: function(opts){
             
             var self = this;
-            self.options.push(opts)
-            return this;
+            self.options.push(opts);
+            return self;
+        },
+        remove: function(elem){
+            var self = this, i = 0, j, len = self.options.length;
+
+            for(; i < len; i++){
+                if(elem.trim() === self.options[i].name.trim()){
+                    j = i;
+                }
+            }
+            self.options.splice(j,1);
+            return self;
         },
         valid: function(){
             var self = this, i = 0, len = self.options.length;
+
             for(; i < len; i++){
                 if(validate.call(self, self.options[i]) === false){
-                    return;
+                    return false;
                 }
             }
+
+            return true;
         }
 
-    }
+    };
     
     win.Mvalidate = Mvalidate;
 
@@ -66,7 +80,7 @@
                 }
 
                 if(!valiReg || !valiStr){
-                    alertMessage(opts.message[i])
+                    alertMessage.call(this, opts, opts.message[i]);
                     return false;
                 }
             }
@@ -74,13 +88,15 @@
             var selfValue = el.value;
             var targetValue = this.form[opts.sameTo].value;
             if(selfValue !== targetValue){
-                alertMessage(opts.message[i])
+                alertMessage.call(this, opts, opts.message[i]);
                 return false;
             }
         }
+
+        return true;
     }
 
-    function alertMessage(message){
+    function alertMessage(opts, message){
         var errorEle = document.createElement('div');
         errorEle.className = 'errorMessage';
         var nodeEles = document.getElementsByClassName('errorMessage');
@@ -88,9 +104,12 @@
         if(nodeEles.length === 0){
             document.getElementsByTagName('body')[0].appendChild(errorEle);
         }
-        var errEl = document.getElementsByClassName('errorMessage')[0]
+        var errEl = document.getElementsByClassName('errorMessage')[0];
         errEl.innerHTML = message;
-        errorMessageStyle(errEl)
+        errorMessageStyle(errEl);
+        if(opts.callback){
+            opts.callback(this.form[opts.name], errEl)
+        }
     }
 
     function errorMessageStyle(errEl){
@@ -99,17 +118,17 @@
         cssStyle.type = 'text/css';
         cssStyle.innerHTML = '.errorMessage{position:fixed;top:50%;right:0;left:0;display:block;margin:auto;padding:5%;width:60%;-webkit-border-radius:4px;background-color:rgba(0,0,0,.7);color:#fff;text-align:center;font-size:16px;transform:translateY(-50%);-ms-transform:translateY(-50%)}.animated{-webkit-animation-duration:2s;animation-duration:2s;-webkit-animation-fill-mode:both;animation-fill-mode:both}@-webkit-keyframes fadeOut{50%{opacity:1}to{opacity:0}}@keyframes fadeOut{50%{opacity:1}to{opacity:0}}.fadeOut{-webkit-animation-name:fadeOut;animation-name:fadeOut}';
 
-        document.head.appendChild(cssStyle)
+        document.head.appendChild(cssStyle);
 
-        errEl.style.display = 'block'
-        errEl.className += ' animated fadeOut'
+        errEl.style.display = 'block';
+        errEl.className += ' animated fadeOut';
 
-        errEl.addEventListener('webkitAnimationEnd',endAnime)
-        errEl.addEventListener('animationend',endAnime)
+        errEl.addEventListener('webkitAnimationEnd',endAnime);
+        errEl.addEventListener('animationend',endAnime);
 
         function endAnime(){
-            removeClass(errEl,'animated')
-            removeClass(errEl,'fadeOut')
+            removeClass(errEl,'animated');
+            removeClass(errEl,'fadeOut');
             errEl.style.display = 'none'
         }
 
@@ -152,3 +171,7 @@
 
 
 }(window, undefined));
+
+
+
+
